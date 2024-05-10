50bbdcde407a7738c1dcbcfb37ce8660f6aae82a
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-10 11:19:25 +0530

    Create Q_6.java

---
+++ Q_6.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Q_6 {
+    public static boolean isOdd(int n){
+        return (n & 1)!=0;
+    }
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int N = sc.nextInt();
+        System.out.println(N +" is odd: "+isOdd(N));
+    }
+}
