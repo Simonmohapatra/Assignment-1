667a8ee29918a06f66c839ff50ae2b6f2255cce5
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-17 20:48:34 +0530

    Q_1.java

--- Q_1.java
+++ Q_1.java
@@ -1,17 +1,17 @@
 public class Q_1 {
 	public static void main(String[] args) {
 		int a = Integer.parseInt(args[0]);
-        if (a>2){
-            System.out.println("The positive integer greater than 2 is: "+a);
-        }
+		if (a>2){
+			System.out.println("The positive integer greater than 2 is: "+a);
+		}
 		else {
-            System.out.println("Invalid input. Please enter a number greater than 2");
-        }
-        int num=a,count=0;
-        while (num>=2){
-            num=num/2;
-            count++;
-        }
-        System.out.println("THe number of  times one must repeatedly divide this number by 2 before getting a value less than 2 is "+count);
-   }
+			System.out.println("Invalid input. Please enter a number greater than 2");
+		}
+		int num=a,count=0;
+		while (num>=2){
+			num=num/2;
+			count++;
+		}
+		System.out.println("THe number of  times one must repeatedly divide this number by 2 before getting a value less than 2 is "+count);
+	}
 }
