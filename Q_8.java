589a2f4007804cf49954bbd28e7857a82c62092e
Simonmohapatra <146565082+Simonmohapatra@users.noreply.github.com>
2024-05-18 13:49:16 +0530

    Q_8.java

--- Q_8.java
+++ Q_8.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class assignment_1 {
+public class Q_8 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of Row and Columns of 2D-Array: ");
